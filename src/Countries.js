fb997c935e46a585f7c98924d5a8a0bf2922336d
Brian Norman <b2norman@uwaterloo.ca>
2020-03-28 12:10:56 -0600

    Countries by percent of population infected

--- src/Countries.js
+++ src/Countries.js
@@ -1,43 +1,56 @@
 import React, { useState, useEffect } from "react"
 import { Bar } from 'react-chartjs-2'
+import populationData from './populations.json'
 
-export default function Test() {
+export default function Countries() {
     const [countries, setCountries] = useState([])
     const [search, setSearch] = useState("")
+    const [countryPopulations, setCountryPopulations] = useState()
+
+    const displayCountries = countries
+                                .filter(country => country.Country.toLowerCase().includes(search.toLowerCase()) && country.TotalConfirmed > 0 && countryPopulations.has(country.Country))
+                                .sort((a, b) => (b.TotalConfirmed/countryPopulations.get(b.Country)) - (a.TotalConfirmed/countryPopulations.get(a.Country)))
+                                .slice(0, 10)
 
     const data = {
-        labels: countries
-                    .filter(country => country.Country.toLowerCase().includes(search.toLowerCase()))
-                    .sort((a, b) => b.TotalConfirmed - a.TotalConfirmed)
-                    .slice(0, 8)
-                    .map(country => country.Country),
+        labels: displayCountries.map(country => country.Country), 
         datasets: [
             {
-                label: 'Confirmed Cases',
+                label: 'Percent Confirmed Cases',
                 backgroundColor: 'rgba(255,99,132,0.2)',
                 borderColor: 'rgba(255,99,132,1)',
                 borderWidth: 1,
                 hoverBackgroundColor: 'rgba(255,99,132,0.4)',
                 hoverBorderColor: 'rgba(255,99,132,1)',
-                data: countries
-                        .filter(country => country.Country.toLowerCase().includes(search.toLowerCase()))
-                        .sort((a, b) => b.TotalConfirmed - a.TotalConfirmed)
-                        .slice(0,8)
-                        .map(country => country.TotalConfirmed)
+                data: displayCountries.map(country => country.TotalConfirmed * 100 /countryPopulations.get(country.Country))
             }
         ]
     }
 
     useEffect(() => {
-        fetch( "https://api.covid19api.com/summary" )
-            .then( res => res.json() )
-            .then( res => setCountries(res.Countries))
-            .catch( error => console.log(error) )
+        fetch("https://api.covid19api.com/summary")
+            .then(res => res.json())
+            .then(res => setCountries(res.Countries))
+            .catch(error => console.log(error))
+
+        let populationMap = new Map()
+        
+        for (const populationDatum of populationData) {
+            if (populationDatum["Country Name"] === "United States") {
+                populationMap.set("US", populationDatum["Value"]);
+            } else if (populationDatum["Country Code"] === "KOR") {
+                populationMap.set("South Korea", populationDatum["Value"])
+            } else {
+                populationMap.set(populationDatum["Country Name"], populationDatum["Value"]);
+            }
+        }
+
+        setCountryPopulations(populationMap)
     }, [])
 
     return (
         <div>
-            <h2>Top Countries by Infected Population</h2>
+            <h2>What Percent of the Population Got Covid-19?</h2>
             <label> Search for country: 
                 <input
                     className = "Search"
